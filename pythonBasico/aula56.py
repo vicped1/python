ef034acea2a84cc35512182efd9bd11c83f6fd5d
vicped1 <149419147+vicped1@users.noreply.github.com>
2026-09-11 21:37:09 +0800

    codigo2

--- pythonBasico/aula56.py
+++ pythonBasico/aula56.py
@@ -4,8 +4,12 @@ Double-precision floating-point format IEEE 754
 https://en.wikipedia.org/wiki/Double-precision_floating-point_format
 https://docs.python.org/pt-br/3/tutorial/floatingpoint.html
 """
-numero_1 = 0.1
-numero_2 = 0.7
+import decimal
+
+decimal.getcontext().prec = 2
+numero_1 = decimal.Decimal('0.1')
+numero_2 = decimal.Decimal('0.7')
 numero_3 = numero_1 + numero_2
 print(numero_3)
 print(f'{numero_3:.2f}')
+print(round(numero_3, 2))
