177359d12b77aa435c5550515f53364bc1f1f2fd
vicped1 <149419147+vicped1@users.noreply.github.com>
2026-09-15 15:46:40 -0300

    traducao

--- pythonBasico/aula59.py
+++ pythonBasico/aula59.py
@@ -7,23 +7,42 @@ python -i mod.py (iterativo com mod)
 
 The Zen of Python, por Tim Peters
 
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
+Bonito é melhor que feio.
+
+Explícito é melhor que implícito.
+
+Simples é melhor que complexo.
+
+Complexo é melhor que complicado.
+
+Plano é melhor que aninhado.
+
+Esparso é melhor que denso.
+
+A legibilidade conta.
+
+Casos especiais não são especiais o suficiente para quebrar as regras.
+
+Embora a praticidade supere a pureza.
+
+Erros nunca devem passar silenciosamente.
+
+A menos que sejam explicitamente silenciados.
+
+Diante da ambiguidade, recuse a tentação de adivinhar.
+
+Deve haver uma — e, de preferência, apenas uma — maneira óbvia de fazer isso.
+
+Embora essa maneira possa não ser óbvia no início, a menos que você seja holandês.
+
+Agora é melhor que nunca.
+
+Embora nunca seja frequentemente melhor do que *agora*.
+
+Se a implementação é difícil de explicar, é uma má ideia.
+
+Se a implementação é fácil de explicar, pode ser uma boa ideia.
+
+Namespaces são uma ideia excelente pra caramba — vamos usar mais deles!
+
 """
